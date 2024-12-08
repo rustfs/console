cc8211224f49a8ef2aee92d7a666a718974fae8f
overtrue <anzhengchao@gmail.com>
2024-12-08 15:21:39 +0800

    init

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,55 +1,42 @@
+
+type FetchType = typeof $fetch
 class ApiClient {
-  private instance: any
+  private $api: any
 
-  constructor(baseURL: string, token?: string, options?: any) {
-    this.instance = $fetch.create({
-      baseURL,
-      onRequest({ request, options }) {
-        if (token) {
-          options.headers.set('Authorization', `Bearer ${token}`)
-        }
-      },
-      onResponse({ response }) {
-        console.log('[API] response', response)
-      },
-      // ...options,
-    })
-  }
-
-  getInstance() {
-    return this.instance
+  constructor(api: FetchType) {
+    this.$api = api
   }
 
   async get(url: string, options?: any) {
-    return this.instance(url, { method: 'GET', ...options })
+    return this.$api(url, { method: 'GET', ...options })
   }
 
   async post(url: string, body: any, options?: any) {
-    return this.instance(url, { method: 'POST', body, ...options })
+    return this.$api(url, { method: 'POST', body, ...options })
   }
 
   async delete(url: string, options?: any) {
-    return this.instance(url, { method: 'DELETE', ...options })
+    return this.$api(url, { method: 'DELETE', ...options })
   }
 
   async put(url: string, body: any, options?: any) {
-    return this.instance(url, { method: 'PUT', body, ...options })
+    return this.$api(url, { method: 'PUT', body, ...options })
   }
 
   async patch(url: string, body: any, options?: any) {
-    return this.instance(url, { method: 'PATCH', body, ...options })
+    return this.$api(url, { method: 'PATCH', body, ...options })
   }
 
   async head(url: string, options?: any) {
-    return this.instance(url, { method: 'HEAD', ...options })
+    return this.$api(url, { method: 'HEAD', ...options })
   }
 
   async options(url: string, options?: any) {
-    return this.instance(url, { method: 'OPTIONS', ...options })
+    return this.$api(url, { method: 'OPTIONS', ...options })
   }
 
   async trace(url: string, options?: any) {
-    return this.instance(url, { method: 'TRACE', ...options })
+    return this.$api(url, { method: 'TRACE', ...options })
   }
 }
 

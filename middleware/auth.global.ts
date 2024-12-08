674bc27c43ed0a94554d7ae08f6cd7ea92b79673
overtrue <anzhengchao@gmail.com>
2024-12-08 15:21:48 +0800

    refactor: api client

---
+++ middleware/auth.global.ts
@@ -0,0 +1,17 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // const token = useLocalStorage('auth.token', undefined)
+
+  // console.log('Auth middleware', to.path, token.value);
+
+  // if (to.path === '/auth/login') {
+  //   if (token.value) {
+  //     return navigateTo('/')
+  //   }
+
+  //   return
+  // }
+
+  // if (!token.value) {
+  //   return navigateTo('/auth/login')
+  // }
+})

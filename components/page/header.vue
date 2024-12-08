d50fdc651b13ecefd671e384ee2c77dd82f63206
overtrue <anzhengchao@gmail.com>
2024-12-08 21:16:46 +0800

    style: page header background

--- components/page/header.vue
+++ components/page/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 px-6 border-b dark:border-gray-700 flex justify-between">
+  <div class="p-4 px-6 border-b dark:border-gray-700 flex justify-between bg-gray-100 dark:bg-neutral-900">
     <slot name="title"></slot>
     <slot name="actions"></slot>
   </div>

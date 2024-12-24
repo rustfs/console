50c2c32c0692498fc7c4adcd09629f0ae364efd0
overtrue <anzhengchao@gmail.com>
2024-12-24 20:15:08 +0800

    feat: 新建文件/文件夹

--- composables/useObject.ts
+++ composables/useObject.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, HeadObjectCommand } from '@aws-sdk/client-s3'
+import { DeleteObjectCommand, GetObjectCommand, HeadObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl as _getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 export function useObject({ bucket, region }: { bucket: string, region?: string }) {
@@ -22,5 +22,24 @@ export function useObject({ bucket, region }: { bucket: string, region?: string 
     return await _getSignedUrl($client, command, { expiresIn })
   }
 
-  return { headObject, getSignedUrl }
+  const putObject = async (key: string, body: Blob | string) => {
+    const params = {
+      Bucket: bucket,
+      Key: key,
+      Body: body
+    }
+
+    return await $client.send(new PutObjectCommand(params))
+  }
+
+  const deleteObject = async (key: string) => {
+    const params = {
+      Bucket: bucket,
+      Key: key
+    }
+
+    return await $client.send(new DeleteObjectCommand(params))
+  }
+
+  return { headObject, putObject, deleteObject, getSignedUrl }
 }

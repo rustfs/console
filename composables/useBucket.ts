9500a4bb913636c30aa6f2270d33c40f3b22fbee
cxymds <cxymds@qq.com>
2024-12-27 15:00:25 +0800

    feaat: bucket

---
+++ composables/useBucket.ts
@@ -0,0 +1,6 @@
+import {} from '@aws-sdk/client-s3';
+import { getSignedUrl as _getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+export function useBucket({ bucket, region }: { bucket: string; region?: string }) {
+  return {};
+}

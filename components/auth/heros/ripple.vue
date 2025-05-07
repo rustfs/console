0f312ef1fec86299f9fa79dfb39b5cd80ad3c932
overtrue <anzhengchao@gmail.com>
2025-05-07 20:51:06 +0800

    i18n: 翻译登录页文案

--- components/auth/heros/ripple.vue
+++ components/auth/heros/ripple.vue
@@ -3,15 +3,16 @@
     <div class="max-w-7xl flex flex-col z-10">
       <img src="~/assets/logo.svg" class="max-w-28" alt="" />
       <div class="text-4xl my-6 font-semibold !text-primary px-0">
-        <span>基于 Rust 的</span>
-        <FlipWords :words="['高性能', '无限扩容', '安全可靠', '多云存储', '兼容 S3']" :duration="3000" class="text-4xl font-semibold !text-primary px-0" />
+        <span :class="{ 'pr-1': locale !== 'zh' }">{{ t('Rust-based') }} </span>
+        <FlipWords :words="[t('High Performance'), t('Infinite Scaling'), t('Secure & Reliable'), t('Multi-Cloud Storage'), t('S3 Compatible')]" :duration="3000"
+          class="text-4xl font-semibold !text-primary px-0" />
         <div class="text-muted-foreground mt-2">
-          靠谱的分布式文件系统
+          {{ t('Reliable distributed file system') }}
         </div>
       </div>
     </div>
-    <a href="https://www.rustfs.com" class="z-10 text-primary-500 inline-flex w-min items-center gap-2 leading-none p-2 px-5 border rounded-full border-blue-500">
-      <span>www.rustfs.com</span>
+    <a href="https://www.rustfs.com" class="z-10 text-primary-500 inline-flex w-max items-center gap-2 leading-none p-2 px-5 border rounded-full border-blue-500">
+      <span>{{ t('Visit website') }}</span>
       <Icon name="ri:arrow-right-long-fill" class="mr-2" />
     </a>
     <div class="h-full inset-0 absolute z-0">
@@ -22,8 +23,9 @@
 </template>
 
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
 import FlipWords from '~/components/ui/flip-words/FlipWords.vue'
 import Ripple from '~/components/ui/ripple/Ripple.vue'
 
-const mode = useColorMode();
+const { t, locale } = useI18n()
 </script>

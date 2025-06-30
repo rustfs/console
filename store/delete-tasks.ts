058e3bb420cbe3574ba8dc9548644383c8a96591
马登山 <cxymds@qq.com>
2025-06-30 09:54:43 +0800

    feat : 修改分片大小为16M

--- store/delete-tasks.ts
+++ store/delete-tasks.ts
@@ -1,32 +1,32 @@
-import { defineStore } from 'pinia';
-import DeleteTaskManager from '~/lib/delete-task-manager';
+import { defineStore } from "pinia";
+import DeleteTaskManager from "~/lib/delete-task-manager";
 
-export const useDeleteTaskManagerStore = defineStore('DeleteTaskManager', {
+export const useDeleteTaskManagerStore = defineStore("DeleteTaskManager", {
   state: () => ({
     taskManager: new DeleteTaskManager(useNuxtApp().$s3Client, {
-      chunkSize: 5,
-      maxConcurrentUploads: 6
-    })
+      chunkSize: 16,
+      maxConcurrentUploads: 6,
+    }),
   }),
   getters: {
-    tasks: (state) => state.taskManager.getTasks()
+    tasks: (state) => state.taskManager.getTasks(),
   },
   actions: {
     addKeys(keys: string[], bucketName: string, prefix?: string) {
-      console.log('addKeys', keys, bucketName, prefix);
-      this.taskManager.addKeys(keys, bucketName, prefix)
-      this.taskManager.start()
+      console.log("addKeys", keys, bucketName, prefix);
+      this.taskManager.addKeys(keys, bucketName, prefix);
+      this.taskManager.start();
     },
     getTasks() {
-      return this.taskManager.getTasks()
+      return this.taskManager.getTasks();
     },
 
     removeTask(taskId: string) {
-      this.taskManager.removeTask(taskId)
+      this.taskManager.removeTask(taskId);
     },
 
     clearTasks() {
-      this.taskManager.clearTasks()
-    }
-  }
-})
+      this.taskManager.clearTasks();
+    },
+  },
+});
--- store/upload-tasks.ts
+++ store/upload-tasks.ts
@@ -1,33 +1,33 @@
-import { defineStore } from 'pinia';
-import UploadTaskManager from '~/lib/upload-task-manager';
+import { defineStore } from "pinia";
+import UploadTaskManager from "~/lib/upload-task-manager";
 
-export const useUploadTaskManagerStore = defineStore('uploadTaskManager', {
+export const useUploadTaskManagerStore = defineStore("uploadTaskManager", {
   state: () => ({
     taskManager: new UploadTaskManager(useNuxtApp().$s3Client, {
-      chunkSize: 5,
-      maxConcurrentUploads: 6
-    })
+      chunkSize: 16,
+      maxConcurrentUploads: 6,
+    }),
   }),
   getters: {
-    tasks: (state) => state.taskManager.getTasks()
+    tasks: (state) => state.taskManager.getTasks(),
   },
   actions: {
     addFiles(files: File[], bucketName: string, prefix?: string) {
-      console.log('addFiles', files, bucketName, prefix);
+      console.log("addFiles", files, bucketName, prefix);
 
-      this.taskManager.addFiles(files, bucketName, prefix)
-      this.taskManager.start()
+      this.taskManager.addFiles(files, bucketName, prefix);
+      this.taskManager.start();
     },
     getTasks() {
-      return this.taskManager.getTasks()
+      return this.taskManager.getTasks();
     },
 
     removeTask(taskId: string) {
-      this.taskManager.removeTask(taskId)
+      this.taskManager.removeTask(taskId);
     },
 
     clearTasks() {
-      this.taskManager.clearTasks()
-    }
-  }
-})
+      this.taskManager.clearTasks();
+    },
+  },
+});

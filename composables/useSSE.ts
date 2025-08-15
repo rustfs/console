90efb6c00b81b91a943e6127c65f7f57ab038fe0
马登山 <cxymds@qq.com>
2025-08-15 14:47:39 +0800

    feat：API

---
+++ composables/useSSE.ts
@@ -0,0 +1,49 @@
+export const useSSE = () => {
+  const { $api } = useNuxtApp();
+  return {
+    // 获取KMS的状态
+    async getKMSStatus() {
+      return await $api.get('/kms/status');
+    },
+
+    // 配置KMS
+    async configureKMS(data: any) {
+      return await $api.post('/kms/configure', data);
+    },
+
+    // 获取配置
+    async getConfiguration() {
+      return await $api.get('/kms/config');
+    },
+
+    // 创建秘钥
+    async createKey(data: any) {
+      return await $api.post('/kms/key/create', data);
+    },
+
+    // 启用秘钥
+    async enableKey(keyName: string) {
+      return await $api.put('/kms/key/enable', {}, { params: { keyName } });
+    },
+
+    // 禁用秘钥
+    async disableKey(keyName: string) {
+      return await $api.put('/kms/key/disable', {}, { params: { keyName } });
+    },
+
+    // 获取秘钥状态
+    async getKeyStatus(params: any) {
+      return await $api.get('/kms/key/status', { params: params });
+    },
+
+    // 获取秘钥列表
+    async getKeyList() {
+      return await $api.get('/kms/key/list');
+    },
+
+    // 删除秘钥
+    async deleteKey(keyName: string) {
+      return await $api.delete('/kms/key/delete', { params: { keyName } });
+    },
+  };
+};

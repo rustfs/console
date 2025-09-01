08c09d9098300cdd4137015763350c9bed34551a
马登山 <cxymds@qq.com>
2025-09-01 17:57:59 +0800

    fix: Dealing situations when bucket does not exist rustfs/rustfs#215

--- pages/browser/[bucket]/[[key]].vue
+++ pages/browser/[bucket]/[[key]].vue
@@ -5,7 +5,7 @@
         <template #title>
           <div class="flex items-center gap-4">
             <h1 @click="$router.push(bucketPath())" class="cursor-pointer">{{ bucketName }}</h1>
-            <object-path-links :object-key="key" @click="(path) => $router.push(bucketPath(path))" />
+            <object-path-links :object-key="key" @click="path => $router.push(bucketPath(path))" />
           </div>
         </template>
       </page-header>
@@ -19,32 +19,48 @@
 </template>
 
 <script lang="ts" setup>
-
-import { useRoute } from '#app'
-import { endsWith } from 'lodash'
-import { joinRelativeURL } from 'ufo'
-import { computed } from 'vue'
+import { useRoute } from '#app';
+import { endsWith } from 'lodash';
+import { joinRelativeURL } from 'ufo';
+import { computed } from 'vue';
 
 // 从路由参数中获取 bucketName, pageSize, continuationToken
-const route = useRoute()
+const route = useRoute();
+
+const message = useMessage();
+const router = useRouter();
 
 // bucketName 和 pageSize 来自路由
-const bucketName = computed(() => route.params.bucket as string)
+const bucketName = computed(() => route.params.bucket as string);
+// 检测bucketName是否存在，使用 head bucket 接口,如果不存在跳转到/browser
+const isBucketExist = () => {
+  return useBucket({ region: route.params.region as string })
+    .headBucket(bucketName.value)
+    .then(res => {
+      return true;
+    })
+    .catch(err => {
+      message.error('Bucket not found');
+      router.push('/browser');
+    });
+};
+
+isBucketExist();
+
 // 当前路径的前缀, example: 'folder1/folder2/'
-const key = computed(() => decodeURIComponent(route.params.key as string))
+const key = computed(() => decodeURIComponent(route.params.key as string));
 
 const bucketPath = (path: string | Array<string> = '') => {
   if (Array.isArray(path)) {
-    path = path.join('/')
+    path = path.join('/');
   }
 
   if (path.length > 1) {
-    path = endsWith(path, '/') ? path : path + '/'
+    path = endsWith(path, '/') ? path : path + '/';
   }
 
-  return joinRelativeURL('/browser', encodeURIComponent(bucketName.value), encodeURIComponent(path))
-}
+  return joinRelativeURL('/browser', encodeURIComponent(bucketName.value), encodeURIComponent(path));
+};
 
-const isObjectList = key.value.endsWith('/') || key.value === ''
-
+const isObjectList = key.value.endsWith('/') || key.value === '';
 </script>

b87133b6adc4759823d285ca45518b22ebab35b3
overtrue <anzhengchao@gmail.com>
2025-10-30 21:29:19 +0800

    feat: add social links

--- components/app-top-nav.vue
+++ components/app-top-nav.vue
@@ -1,12 +1,14 @@
 <template>
   <header
-    class="flex h-16 shrink-0 items-center gap-2 justify-between border-b bg-background px-4 transition-[height] ease-linear backdrop-blur group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    class="flex h-16 shrink-0 items-center gap-2 justify-between border-b bg-background transition-[height] ease-linear backdrop-blur group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
     <div class="flex items-center gap-2">
       <SidebarTrigger class="-ml-1" />
     </div>
     <div class="flex items-center gap-2">
       <LanguageSwitcher />
       <ThemeSwitcher />
+      <LinksGithub />
+      <LinksX />
       <UserDropdown />
     </div>
   </header>

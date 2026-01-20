e01453cfb9bf5a1359276ae456fcd4bb7d8058c2
Dan <nakrovati@outlook.com>
2026-01-21 00:19:07 +0400

    fix: PageHeader overlaps Sidebar on small screens

--- app.config.ts
+++ app.config.ts
@@ -5,4 +5,8 @@ export default defineAppConfig<AppConfig>({
   navs: navs,
   name: process.env.APP_NAME || 'RustFS',
   description: process.env.APP_DESCRIPTION || 'RustFS is a distributed file system written in Rust.',
+  icon: {
+    mode: 'css',
+    cssLayer: 'base'
+  }
 })

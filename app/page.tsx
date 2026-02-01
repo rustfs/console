f9966dc0f22fad6a221ca18b1d40f0ab39674e01
overtrue <anzhengchao@gmail.com>
2026-02-01 21:45:57 +0800

    chore: remove root page

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function RootPage() {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "/rustfs/console"
-  redirect(basePath)
-}
